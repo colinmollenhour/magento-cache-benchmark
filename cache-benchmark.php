a553a5636b99f538e8408da959f7f7e44730186c
colinmollenhour <colin@mollenhour.com>
2011-09-14 19:57:51 -0400

    Overhaul for general-purpose cache benchmarking.

--- cache-benchmark.php
+++ cache-benchmark.php
@@ -30,14 +30,7 @@
 
 // use __FILE__ instead of __DIR__ because php 5.3 isn't available everywhere yet
 // This strange require lets the script work from my modman location, too
-$sentry = 0;
-$abstract = '/../../../../../../shell/abstract.php';
-while (! file_exists(dirname(__FILE__) . $abstract) && $sentry < 2) {
-  $abstract = '/..' . $abstract;
-  $sentry++;
-}
-$abstract = '/abstract.php';
-require_once dirname(__FILE__) . $abstract;
+require_once 'shell/abstract.php';
 
 class benchmark extends Mage_Shell_Abstract
 {
@@ -59,80 +52,347 @@ class benchmark extends Mage_Shell_Abstract
   protected $_cachePrefix = '';
 
   /**
-   * Evaluate arguments and run benchmark.
-   *
-   * @return benchmark
+   * Evaluate arguments and run or init benchmark.
    */
   public function run()
   {
-    $this->_echoCacheConfig();
+    if($this->getArg('init')) {
+      $this->_initDataset();
+    }
+    else if($this->getArg('clean')) {
+      Mage::app()->getCache()->clean();
+    }
+    else if($this->getArg('load')) {
+      $this->_echoCacheConfig();
+      $this->_loadDataset();
+    }
+    else if($this->getArg('tags')) {
+      $this->_runTagBenchmark();
+    }
+    else if($this->getArg('ops')) {
+      $this->_runOpsBenchmark();
+    }
+    else {
+      echo $this->usageHelp();
+    }
+  }
 
-    $nEntries = (int) $this->getArg('init');
+  protected function _getTestDir()
+  {
+    $name = $this->getArg('name') ?: 'default';
+    $baseDir = Mage::getBaseDir('var').'/cachebench';
+    if( ! is_dir($baseDir)) {
+      mkdir($baseDir);
+    }
+    return $baseDir.'/'.$name;
+  }
+
+  /**
+   * Initialize a dataset to files on disk (not loaded into cache)
+   * 
+    --keys <num>          Number of cache keys (default to 10000)
+    --tags <num>          Number of cache tags (default to 2000)
+    --min-tags <num>      The min number of tags to use for each record (default 0)
+    --max-tags <num>      The max number of tags to use for each record (default 15)
+    --min-rec-size <num>  The smallest size for a record (default 1)
+    --max-rec-size <num>  The largest size for a record (default 1024)
+    --clients <num>       The number of clients for multi-threaded testing (defaults to 4)
+    --ops <num>           The number of operations per client (defaults to 10000)
+    --write-chance        The chance-factor that a key will be overwritten (defaults to 1000)
+    --clean-chance        The chance-factor that a tag will be cleaned (defaults to 5000)
+    --seed <num>          The random number generator seed (default random)
+   */
+  protected function _initDataset()
+  {
+    $name = $this->getArg('name') ?: 'default';
+    $testDir = $this->_getTestDir();
+    if (is_dir($testDir)) {
+      array_map('unlink', glob("$testDir/*.*"));
+    }
+    if ( ! is_dir($testDir)) {
+      mkdir($testDir);
+    }
+
+    // Dump command-line
+    file_put_contents("$testDir/cli.txt", 'php '.implode(' ',$_SERVER['argv']));
+
+    $numKeys = $this->getArg('keys') ?: 10000;
+    $numTags = $this->getArg('tags') ?: 2000;
+    $minTags = $this->getArg('min-tags') ?: 0;
+    $maxTags = $this->getArg('max-tags') ?: 15;
+    $minSize = $this->getArg('min-rec-size') ?: 1;
+    $maxSize = $this->getArg('max-rec-size') ?: 1024;
+    $numClients = $this->getArg('clients') ?: 4;
+    if($this->getArg('seed')) {
+      mt_srand( (int) $this->getArg('seed'));
+    }
+    $numOps = $this->getArg('ops') ?: 100000;
+    $writeFactor = $this->getArg('write-chance') ?: 1000;
+    $cleanFactor = $this->getArg('clean-chance') ?: 5000;
+
+    $data = array();
+    $tags = array();
+    $lengths = array();
+    $reads = array();
+    $writes = array();
+    $expires = array(false,false,false,false,false,'rand'); // 1/6th of keys expire
+
+    echo "Generating cache data...\n";
+    $progressBar = $this->_getProgressBar(0, $numKeys / 100);
+
+    // Generate tags
+    $this->_createTagList($numTags);
+
+    // Generate data
+    for($i = 0; $i < $numKeys; $i++) {
+      if($i % 100 == 0) {
+        $progressBar->update(($i / 100)+1);
+      }
+      $key = md5($i);
+      $expireAt = $expires[mt_rand(0,count($expires)-1)];
+      if($expireAt == 'rand') {
+        $expireAt = mt_rand(10, 14400);
+      }
+      $data[$key] = array(
+        'data' => $this->_getRandomData($minSize, $maxSize),
+        'tags' => $this->_getRandomTags($minTags, $maxTags),
+        'expires' => $expireAt
+      );
+
+      // Store length since data length per key will usually be constant
+      $lengths[$key] = strlen($data[$key]['data']);
+      $tags[$key] = $data[$key]['tags'];
+
+      // Some keys are read more frequently
+      $popularity = mt_rand(1,100);
+      for($j = 0; $j < $popularity; $j++) {
+        $reads[] = $key;
+      }
+
+      // Some keys are written more frequently
+      $volatility = mt_rand(1,100);
+      for($j = 0; $j < $volatility; $j++) {
+        $writes[] = $key;
+      }
+    }
+    $progressBar->finish();
+
+    // Dump data
+    file_put_contents("$testDir/data.json", json_encode($data));
+    $data = NULL;
+
+    echo "Generating operations...\n";
+    $progressBar = $this->_getProgressBar(0, ($numClients * $numOps) / 1000);
+
+    // Create op lists for each client
+    for($i = 0, $k = 0; $i < $numClients; $i++)
+    {
+      $ops = array();
+      for($j = 0; $j < $numOps; $j++)
+      {
+        if($k++ % 1000 === 0) {
+          $progressBar->update($k / 1000);
+        }
+
+        // Clean
+        if(mt_rand(0, $cleanFactor) == 0) {
+          $index = mt_rand(0,count($this->_tags)-1) ;
+          $tag = $this->_tags[$index];
+          $ops[] = array('clean', $tag);
+        }
+
+        // Write
+        else if(mt_rand(0, $writeFactor) == 0) {
+          $index = mt_rand(0,count($writes)-1);
+          $key = $writes[$index];
+          $ops[] = array('write', $key, $lengths[$key], $tags[$key]);
+        }
+
+        // Read
+        else {
+          $index = mt_rand(0,count($reads)-1);
+          $key = $reads[$index];
+          $ops[] = array('read', $key);
+        }
+      }
+      file_put_contents("$testDir/ops-client_$i.json", json_encode($ops));
+    }
+    $progressBar->finish();
+
+    $quiet = $numClients == 1 ? '':'--quiet';
+    $script = <<<BASH
+#!/bin/bash
+if [ "$1" != "keep" ]; then
+  php shell/cache-benchmark.php clean
+  php shell/cache-benchmark.php load --name '$name'
+fi
+php shell/cache-benchmark.php tags
+results=var/cachebench/$name/results.txt
+rm -f \$results
+
+clients=0
+function runClient() {
+  clients=$((clients+1))
+  php shell/cache-benchmark.php ops --name '$name' --client \$1 $quiet >> \$results &
+}
+start=$(date '+%s')
+BASH;
+    $script .= "\n";
+    for($i = 0; $i < $numClients; $i++) {
+      $script .="runClient $i\n";
+    }
+    $script .= <<<BASH
+wait
+finish=$(date '+%s')
+elapsed=$((finish - start))
+echo "\$clients concurrent clients completed in \$elapsed seconds"
+echo ""
+echo "         |   reads|  writes|  cleans"
+echo "------------------------------------"
+awk '
+BEGIN { FS=OFS="|" }
+      { print; for (i=2; i<=NF; ++i) { sum[i] += \$i; total += \$i; } j=NF }
+END   {
+        printf "------------------------------------\\n";
+        printf "%9.2f", total;
+        for (i=2; i <= j; ++i) printf "%s%8.2f", OFS, sum[i];
+      }
+' \$results
+echo ""
+BASH;
+    file_put_contents("$testDir/run.sh", $script);
+
+    echo "Completed generation of test data for test '$name'.\n";
+    echo "Run your test like so:\n\n";
+    echo "  \$ bash var/cachebench/$name/run.sh\n";
+  }
+
+  /**
+   * Load the specified dataset
+   */
+  protected function _loadDataset()
+  {
+    $name = $this->getArg('name') ?: 'default';
+    $testDir = $this->_getTestDir();
+    $dataFile = "$testDir/data.json";
+    if( ! file_exists($dataFile)) {
+      throw new RuntimeException("The '$name' test data does not exist. Please run the 'init' command.");
+    }
+    echo "Loading '$name' test data...\n";
+    $data = json_decode(file_get_contents($dataFile), true);
+    $progressBar = $this->_getProgressBar(0, count($data) / 100);
+    $i = 0;
+    $start = microtime(true);
+    $size = 0;
+    foreach($data as $key => $cache) {
+      if($i++ % 100 == 0) {
+        $progressBar->update($i / 100);
+      }
+      $size += strlen($cache['data']);
+      Mage::app()->saveCache($cache['data'], $key, $cache['tags'], $cache['expires']);
+    }
+    $elapsed = microtime(true)-$start;
+    $progressBar->finish();
+    printf("Loaded %d cache records in %.4f seconds. Total size is %d bytes\n", $i, $elapsed, $size);
+  }
+
+  /**
+   * Run the tags benchmark on the existing dataset
+   */
+  protected function _runTagBenchmark()
+  {
+
     $verbose  = $this->getArg('v') || $this->getArg('verbose');
+    printf('Analyzing current cache contents (please be patient)... ');
+    $start = microtime(true);
 
-    if ($nEntries)
+    $nTags = $this->_readTags();
+    if (1 > $nTags)
     {
-
-      if (0 === ($nTags = (int) $this->getArg('tag')))
-      {
-        $nTags = (int) $this->getArg('tags');
-      }
-      $minTags   = (int) $this->getArg('min');
-      $maxTags   = (int) $this->getArg('max');
-
-      if ($nEntries < 1)
-      {
-        throw new InvalidArgumentException('Number of records to create must be one or more');
-      }
-      if ($nTags < 1) $nTags = 30;
-      if ($minTags < 1) $minTags = 5;
-      if ($maxTags < 1) $maxTags = $minTags + 5;
-      if ($maxTags > $nTags) $maxTags = $nTags;
-
-      if ($minTags != $maxTags)
-      {
-        $rangeString = sprintf('%d-%d', $minTags, $maxTags);
-      }
-      else
-      {
-        $rangeString = $minTags;
-      }
-
-      $this->_println(sprintf('Clearing & Initialising cache with %d records using %s out of %d tags per record...',
-        $nEntries, $rangeString, $nTags
-      ));
-      Mage::app()->cleanCache();
-      $this->_createTagList($nTags);
-      $this->_createCacheRecords($nEntries, $minTags, $maxTags);
-
+      throw new OutOfRangeException('No cache tags found in cache');
     }
-    else
+    $nEntries = $this->_countCacheRecords();
+    if (1 > $nEntries)
     {
-      printf('Analyzing current cache contents (please be patient)... ');
-      $start = microtime(true);
-
-      $nTags = $this->_readTags();
-      if (1 > $nTags)
-      {
-        throw new OutOfRangeException('No cache tags found in cache');
-      }
-      $nEntries = $this->_countCacheRecords();
-      if (1 > $nEntries)
-      {
-        throw new OutOfRangeException('No cache records found in cache');
-      }
-
-      $time = microtime(true) - $start;
-      $this->_println(sprintf('%d cache IDs and %d cache tags read in %ss', $nEntries, $nTags, $time));
+      throw new OutOfRangeException('No cache records found in cache');
     }
 
-    $this->_println(sprintf('Benchmarking %d cache records with %d tags', $nEntries, $nTags));
+    $time = microtime(true) - $start;
+    echo sprintf('%d cache IDs and %d cache tags read in %ss', $nEntries, $nTags, $time)."\n";
+
+    echo sprintf('Benchmarking %d cache records with %d tags', $nEntries, $nTags)."\n";
 
     $times = $this->_benchmarkByTag($verbose);
 
     $this->_echoAverage($times);
+  }
 
-    return $this;
+  /**
+   * Run the ops benchmark from the specified dataset
+   */
+  protected function _runOpsBenchmark()
+  {
+    $quiet  = $this->getArg('q') || $this->getArg('quiet');
+
+    $name = $this->getArg('name') ?: 'default';
+    $testDir = $this->_getTestDir();
+    $client = $this->getArg('client') ?: 0;
+    $dataFile = "$testDir/ops-client_$client.json";
+    if( ! file_exists($dataFile)) {
+      throw new RuntimeException("The '$name' test data does not exist. Please run the 'init' command.");
+    }
+    if( ! $quiet) echo "Loading operations...\n";
+    $data = json_decode(file_get_contents($dataFile), true);
+    if( ! $quiet) {
+      echo "Executing operations...\n";
+      $progressBar = $this->_getProgressBar(0, count($data) / 100);
+    }
+    $times = array(
+      'read_time' => 0,
+      'reads' => 0,
+      'write_time' => 0,
+      'writes' => 0,
+      'clean_time' => 0,
+      'cleans' => 0,
+    );
+    foreach($data as $i => $op)
+    {
+      switch($op[0])
+      {
+        case 'read':
+          $start = microtime(TRUE);
+          Mage::app()->loadCache($op[1]);
+          $elapsed = microtime(TRUE) - $start;
+          break;
+        case 'write':
+          $string = $this->_getRandomData($op[2]);
+          $start = microtime(TRUE);
+          Mage::app()->saveCache($string, $op[1], $op[3]);
+          $elapsed = microtime(TRUE) - $start;
+          break;
+        case 'clean':
+          $start = microtime(TRUE);
+          Mage::app()->cleanCache($op[1]);
+          $elapsed = microtime(TRUE) - $start;
+          break;
+        default:
+          throw new RuntimeException('Invalid op: '.$op[0]);
+      }
+      $times[$op[0].'_time'] += $elapsed;
+      $times[$op[0].'s'] += 1;
+      if( ! $quiet && ($i % 100 == 0)) {
+        $progressBar->update($i / 100);
+      }
+    }
+    if( ! $quiet) {
+      $progressBar->finish();
+    }
+
+    printf("Client %2d", $client);
+    foreach(array('read','write','clean') as $op) {
+      printf("|%8.2f", $times[$op.'s'] / $times[$op.'_time']);
+    }
+    echo "\n";
   }
 
   /**
@@ -265,37 +525,11 @@ class benchmark extends Mage_Shell_Abstract
   }
 
   /**
-   * Create the given number of cache records.
-   * Each record only contains the sample data "X".
-   * Each records is associated with a random number of cache tags between the
-   * given min and max.
-   *
-   * @param int $nEntries The number of Cache records to create
-   * @param int $minTags The minimum number of cache tags for each record
-   * @param int $maxTags The maximum number of cache tags for each record
-   */
-  protected function _createCacheRecords($nEntries, $minTags, $maxTags)
-  {
-    $data = 'X';
-    $progressBar = $this->_getProgressBar(0, $nEntries);
-
-    $start = microtime(true);
-    for ($i = 0; $i < $nEntries; $i++)
-    {
-      $id = sprintf('rec_%010d', $i);
-      $tags = $this->_getRandomTags($minTags, $maxTags);
-      Mage::app()->saveCache($data, $id, $tags);
-      $progressBar->update($i+1);
-    }
-    $time = microtime(true) - $start;
-    $progressBar->finish();
-    $this->_println(sprintf('Initialization finished in %ss', $time));
-  }
-
-  /**
    * Since many operations can take quite a long time for large cache pools,
    * this might help ease the waiting time with a nice console progress bar.
    *
+   * @param $min
+   * @param $max
    * @return Zend_ProgressBar A fresh Zend_ProgressBar instance
    */
   protected function _getProgressBar($min, $max)
@@ -324,6 +558,27 @@ class benchmark extends Mage_Shell_Abstract
   }
 
   /**
+   * Get random data for cache
+   * 
+   * @param $min
+   * @param $max
+   * @return string
+   */
+  protected function _getRandomData($min, $max = NULL)
+  {
+    if($max === NULL) {
+      $length = $min;
+    } else {
+      $length = mt_rand($min, $max);
+    }
+    $string = md5(mt_rand());
+    while(strlen($string) < $length) {
+      $string = base64_encode($string);
+    }
+    return substr($string, 0, $length);
+  }
+
+  /**
    * Display the given string with a trailing newline.
    *
    * @param string $msg The string to display
@@ -348,7 +603,7 @@ class benchmark extends Mage_Shell_Abstract
 
     if (! $verbose)
     {
-      $progressBar = $this->_getProgressBar(0, count($this->_tags));
+      $progressBar = $this->_getProgressBar(0, count($this->_tags) / 10);
       $counter = 0;
     }
 
@@ -364,7 +619,9 @@ class benchmark extends Mage_Shell_Abstract
       }
       else
       {
-        $progressBar->update(++$counter);
+        if($counter++ % 10 == 0) {
+          $progressBar->update($counter / 10);
+        }
       }
     }
     if (! $verbose)
@@ -420,17 +677,27 @@ class benchmark extends Mage_Shell_Abstract
   public function usageHelp()
   {
     return <<<USAGE
-This scrit initialize all cache tag symlinks.
-The script needs read/write permissions to access the magento cache files.
+This script will either initialize a new benchmark dataset or run a benchmark.
 
-Usage:  php -f app/code/local/Netzarbeiter/Cache/shell/benchmark.php -- [options]
+Usage:  php -f shell/cache-benchmark.php [command] [options]
 
-    --init <num> Clear existing cache records and create <num> entries
-    --tag <num>  If init was used, specify the number of tags to create (default to 30)
-  --min <num>  If init was used, the min number of tags to use for each record (default 5)
-  --max <num>  If init was used, the max number of tags to use for each record (default min +5)
-  -v           Display statistics for every cache tag
-  --help       This help
+Commands:
+  init (See "Initialization options")
+  clean
+  load --name <string>
+  tags
+  ops --name <string> --client <num> [-q|--quiet]
+
+Initialization options:
+  --name <string>       A unique name for this dataset (default to "default")
+  --keys <num>          Number of cache keys (default to 10000)
+  --tags <num>          Number of cache tags (default to 2000)
+  --min-tags <num>      The min number of tags to use for each record (default 0)
+  --max-tags <num>      The max number of tags to use for each record (default 15)
+  --min-rec-size <num>  The smallest size for a record (default 1)
+  --max-rec-size <num>  The largest size for a record (default 1024)
+  --clients <num>       The number of clients for multi-threaded testing (defaults to 4)
+  --seed <num>          The random number generator seed (default random)
 
 USAGE;
   }
